f59510b142e5b9d71843162a4fad3c168608977f
Aramuni <warley.maer@gmail.com>
2021-02-12 13:23:07 -0300

    .

--- script.js
+++ script.js
@@ -4,5 +4,20 @@ $(document).ready(function () {
        window.location.href = 'www.google.com';
    })
 
+    var alexaClient;
+    Alexa.create({version: '1.1'})
+        .then((args) => {
+            const {
+                alexa,
+                message
+            } = args;
+            alexaClient = alexa;
+            document.getElementById('debugElement').innerHTML = 'Alexa is ready :)';
+        })
+        .catch(error => {
+            document.getElementById('debugElement').innerHTML = 'Alexa not ready :(';
+        });
+
+
 });
 
