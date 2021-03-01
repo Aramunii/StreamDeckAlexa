25f43adff442af9fee652dcb46f78e2720ef02af
Aramuni <warley.maer@gmail.com>
2021-03-01 20:59:22 -0300

    teste

--- script.js
+++ script.js
@@ -110,12 +110,17 @@ function getPcStatus(time,socket) {
 }
 
 function setWowData(data) {
-    var maxHealth = data[1]
-    var health = data[0]
-    var locationMap = data[2];
-    console.log('teste')
-    $('#teste2').text(locationMap)
-    var barhealth = health / maxHealth ;
-    bar.animate(barhealth);  // Number from 0.0 to 1.0
+    try{
+        var maxHealth = data[1]
+        var health = data[0]
+        var locationMap = data[2];
+        console.log('teste')
+        $('#teste2').text(locationMap)
+        var barhealth = health / maxHealth ;
+        bar.animate(barhealth);  // Number from 0.0 to 1.0
+    }catch (e) {
+        swal.fire(e.message, '', 'error')
+    }
+
 
 }
